9f7715a288a99eb093a52007e3efddd9290a3f0a
Juan <juanv87@gmail.com>
2022-09-19 11:58:57 -0300

    Normalizar nombres de funciones

--- components/Elements/ListaRespuestas/ListaRespuestas.tsx
+++ components/Elements/ListaRespuestas/ListaRespuestas.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../../hooks";
 // import { startLoadingRespuestas } from "../../../store/entries";
-import { Respuesta } from "../RespuestaCard/RespuestaCard";
+import { RespuestaCard } from "../RespuestaCard/RespuestaCard";
 import LoadingSpinner from "../../Loaders/LoadingSpinner/LoadingSpinner";
 import styles from "./Respuestas.module.scss";
 import { IRespuesta } from "../../../types/IRespuesta";
 
 interface Props {
-  respuestas: IRespuesta[]
+  respuestas: IRespuesta[];
 }
 
 export const ListaRespuestas = ({ respuestas }: Props) => {
@@ -15,7 +15,7 @@ export const ListaRespuestas = ({ respuestas }: Props) => {
     <section className={styles.respuestasContainer}>
       {respuestas &&
         respuestas.map(({ id, titulo, autor }) => (
-          <Respuesta key={id} titulo={titulo} autor={autor} />
+          <RespuestaCard key={id} titulo={titulo} autor={autor} />
         ))}
     </section>
   );
