dd97baffef8c578b3e626004ea56da2b3c30f2c5
César Carabetta <carabetta87@gmail.com>
2022-09-25 17:56:09 -0300

    Abstraida la logica del delete en preguntacard

--- hooks/useDelete.tsx
+++ hooks/useDelete.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { useAppDispatch } from '../hooks';
 import { startRemovingSavedPregunta } from '../store/entries';
+import { IPregunta } from '../types/IPregunta';
 
+interface Props {
+  pregunta: IPregunta;
+  setSavedPregunta: Function;
+  setUpdatedSaved: Function;
+  updatedSaved: Boolean;
+}
 
-const useDelete = () => {
+const useDelete = ({pregunta, setSavedPregunta, setUpdatedSaved, updatedSaved}: Props) => {
     
     const dispatch = useAppDispatch()
-    const onDeleteSavedPregunta = async (pregunta:any, setSavedPregunta:any, setUpdatedSaved:any, updatedSaved:any) => {
+    const onDeleteSavedPregunta = async () => {
         await dispatch(startRemovingSavedPregunta({ pregunta }));
         setSavedPregunta(false);
         dispatch(setUpdatedSaved(!updatedSaved));
