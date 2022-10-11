fa1af8e3a1cf5aee2237f2a8ebc36ed4a85bf3c4
César Carabetta <carabetta87@gmail.com>
2022-10-10 22:47:40 -0300

    Agregado funcion de likear preguntas

--- components/Elements/ListaPreguntas/ListaPreguntas.tsx
+++ components/Elements/ListaPreguntas/ListaPreguntas.tsx
@@ -1,13 +1,21 @@
-import React from "react";
-import { useAppSelector } from "../../../hooks";
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../../hooks";
+import { getLikedByUser } from "../../../store/entries/thunks";
 import { IPregunta } from "../../../types/IPregunta";
 import LoadingSpinner from "../../Loaders/LoadingSpinner/LoadingSpinner";
 import { PreguntaCard } from "../PreguntaCard/PreguntaCard";
 import styles from "./ListaPreguntas.module.scss";
+
 export const ListaPreguntas = () => {
+  const dispatch = useAppDispatch()
   const { preguntas, isLoadingPreguntas } = useAppSelector(
     (state) => state.entries
     );
+  const { uid } = useAppSelector((state) => state.auth);
+  
+  useEffect(()=>{
+    uid && dispatch(getLikedByUser(uid))
+  }, [uid])
   console.log("🚀 ~ file: ListaPreguntas.tsx ~ line 9 ~ ListaPreguntas ~ isLoadingPreguntas", isLoadingPreguntas)
   return (
     <>
---
+++ components/ui/ButtonStyled/ButtonStyled.tsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+// const ButtonStyled = ({}) => {
+//   return (
+//     <button 
+//           className="respuestasContainer__button--menos"
+//           onClick={handlePaginateMinus}
+//           >Ver Menos</button>
+//   )
+// }
+
+// export default ButtonStyled
