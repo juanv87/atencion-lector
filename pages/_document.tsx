02a3ca4d65fd1362ce419474951c9657c28c626d
César Carabetta <carabetta87@gmail.com>
2022-10-16 16:42:14 -0300

    agregados estilos a saved preguntas

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&family=Open+Sans:wght@300;400;600;800&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&family=Open+Sans:wght@300;400;600;800&family=Lato:wght@300;400;700&family=Montserrat:wght@300;400;500&family=Nunito+Sans:wght@300;400;600&family=Mukta:wght@300;400;500&family=Quicksand:wght@300;400;500&display=swap"
           rel="stylesheet"
         />
         <link
