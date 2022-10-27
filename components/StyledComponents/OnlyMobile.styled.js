ba4b83201a616c932736fd4d1b94a3c690afe357
juanv87 <juanv87@gmail.com>
2022-10-26 21:13:49 -0300

    Estilos mobile Perfil

--- components/StyledComponents/OnlyMobile.styled.js
+++ components/StyledComponents/OnlyMobile.styled.js
@@ -1,13 +1,12 @@
 import styled from "styled-components";
 
 export const OnlyMobile = styled.div`
-  @media (max-width: 1024px) {
-    display: none;
-  }
-  @media (max-width: 768px) {
+  && {
     display: none;
   }
   @media (max-width: 480px) {
-    display: block;
+    && {
+      display: block;
+    }
   }
 `;
