ab5f7ed05c7ed7af0fd12e86c8e989bd00c795ae
César Carabetta <carabetta87@gmail.com>
2022-11-10 00:27:01 -0300

    Diseño de cards mensajes

--- components/Elements/MisMensajes/MisMensajes.tsx
+++ components/Elements/MisMensajes/MisMensajes.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '../../../hooks';
 import { loadUserMessages } from '../../../store/userMessages/thunks'
+import { TbMessage2 , TbWriting } from 'react-icons/tb'
+import styles from './MisMensajes.module.scss'
 
 const MisMensajes = () => {
     const { user: { mensajes } } = useAppSelector( (state) => state.userMessages );
@@ -12,13 +14,21 @@ const MisMensajes = () => {
     
   return (
     <>
-      <div className='mensajes'>
+      <div className={styles.mensajes}>
         { mensajes.length > 0 ? mensajes.map( msg => {
             return (
             <>
-              <div className="mensajes_card">
-                <span>De: {msg.autor}</span>
-                <p>Mensaje: {msg.texto}</p>
+            <div className={styles.mensajes_up}>
+                <TbWriting/>
+                <span>{msg.autor}:</span>
+            </div>
+              <div className={styles.mensajes_card}>
+                <div className={styles.mensajes_card__left}>
+                  <TbMessage2/>
+                </div>
+                <div className={styles.mensajes_card__right}>
+                  <p>{msg.texto}</p>
+                </div>
               </div>
             </>
             )
