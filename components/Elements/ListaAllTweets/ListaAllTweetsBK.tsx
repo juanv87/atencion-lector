ef1b61ddaf03e09b586bcf93df2b8c153a85e961
Juan <juanv87@gmail.com>
2023-01-03 14:53:17 -0300

    Implementacion de ReactQuery para manejar los tweets

--- components/Elements/ListaAllTweets/ListaAllTweetsBK.tsx
+++ components/Elements/ListaAllTweets/ListaAllTweetsBK.tsx
@@ -7,23 +7,22 @@ import LoadingSpinner from "../../Loaders/LoadingSpinner/LoadingSpinner";
 import { PreguntaCard } from "../PreguntaCard/PreguntaCard";
 import { TweetCard } from "../TweetCard/TweetCard";
 import styles from "./ListaAllTweets.module.scss";
-import { useQuery } from "react-query";
 
-export const ListaAllTweets = ({ limit }: { limit: number }) => {
-  const { isLoading, error, data } = useQuery("listTweets", () =>
-    fetch(`${process.env.NEXT_PUBLIC_URL_PROD}/api/tweets?limit=${limit}`).then(
-      (res) => res.json()
-    )
-  );
+export const ListaAllTweets = () => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+      dispatch(startLoadingTweets(5));
+  }, []);
+  
+  const { tweets } = useAppSelector((state) => state.tweets);
+  const sorted = [...tweets];
 
+  const tweetsSorted = sorted.sort((a, b) => {
+    return b.createdAt - a.createdAt;
+  });
   return (
     <section className={styles.listaTweets}>
-      {isLoading ? (
-        <LoadingSpinner />
-      ) : error ? (
-        <div>Something went wrong</div>
-      ) : null}
-      {data?.tweets.map((tweet: ITweet) => {
+      {tweets.length <= 1 ? <LoadingSpinner /> : tweetsSorted.map((tweet: ITweet) => {
         return <TweetCard key={tweet.id} tweet={tweet} />;
       })}
     </section>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,17 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "../store/";
+
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </Provider>
   );
 }
