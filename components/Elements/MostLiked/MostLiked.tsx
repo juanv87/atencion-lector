97059e766fd56eaf38b1960771191c900b40d374
Juan <juanv87@gmail.com>
2023-01-03 17:34:15 -0300

    Endpoint preguntas mas likeadas. Componente MostLiked con RQ

--- components/Elements/MostLiked/MostLiked.tsx
+++ components/Elements/MostLiked/MostLiked.tsx
@@ -4,19 +4,14 @@ import styles from "./MostLiked.module.scss";
 import LoadingSpinner from "../../Loaders/LoadingSpinner/LoadingSpinner";
 import { IPregunta } from "../../../types/IPregunta";
 import { SideCard } from "../SideCard/SideCard";
+import { useQuery } from "react-query";
 
 const MostLiked = () => {
-  const { preguntas, isLoadingPreguntas } = useAppSelector(
-    (state) => state.entries
+  const { isLoading, error, data } = useQuery("listPreguntas", () =>
+    fetch(`${process.env.NEXT_PUBLIC_URL_PROD}/api/preguntas/likes`).then(
+      (res) => res.json()
+    )
   );
-
-  const sorted = [...preguntas];
-
-  const mostLiked = sorted.sort((a, b) => {
-    let x = a.likes,
-      y = b.likes;
-    return x < y ? 1 : x > y ? -1 : 0;
-  });
 
   return (
     <>
@@ -25,15 +20,15 @@ const MostLiked = () => {
           <div className={styles.mostLikedTitle}>
             <h2>Las preguntas más likeadas</h2>
           </div>
-          {isLoadingPreguntas && (
+          {isLoading && (
             <>
               <div className={styles.spinner}>
                 <LoadingSpinner />
               </div>
             </>
           )}
-          {mostLiked.length > 1 &&
-            mostLiked
+          {data?.preguntas.length > 1 &&
+            data?.preguntas
               .slice(0, 5)
               .map((pregunta: IPregunta) => (
                 <SideCard
